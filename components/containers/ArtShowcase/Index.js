ca985da98b96bb7ca93fa5edb76390f375b2a92d
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2023-03-13 19:24:33 -0600

    updated art showcase

--- components/containers/ArtShowcase/Index.js
+++ components/containers/ArtShowcase/Index.js
@@ -3,52 +3,67 @@ import SwiperCore, { Navigation, Pagination, Controller, Thumbs } from 'swiper'
 import 'swiper/swiper-bundle.css'
 import {Image,Text,Box,} from '@chakra-ui/react';
 
+
+
+
+
+// REACT SLICK
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+// import Slider from "react-slick";
 SwiperCore.use([Navigation]);
 
 
 const Index = () => {
   return (
     <>
-    <Box
-    display='flex'
-    flexDirection='column'
-    textAlign='center'
-    justifyContent='center'
-    alignItems='center'
-    position='relative'
-    top='30px'
-    height='50%'
-    >
+    
 
         <Text as='h1'
         fontFamily='poppins'
         fontSize='28px'
         mb='35px'
         fontWeight='bold'
-        
-        
+        textAlign='center'
+        style={{
+            paddingTop: ['150px']
+        }}
         
         >Check Out Our Monthly Features!</Text>
     <Swiper 
-
+style={{
+    paddingTop: ['50px'],
+    background: 'pink',
+    marginBottom: ['190px'],
+    padding:[null,null,'190px','190px', '190px'],
+    display: ['flex']
+}}
     ScreensPerView={2}
     spaceBetween={10}
     >
-      <SwiperSlide className='swiper'><Image src='./CarouselOne.jpeg' alt='Carousel Image' />
-      <Text as='h1' fontSize='20px'>Skater</Text>
-        <Text as='p'>By: <br/> Peter Delira</Text>
+      <SwiperSlide className='swiper'><img src='./CarouselOne.jpeg' alt='Carousel Image'/>
+      <Box textAlign='center'>
+      <Text as='h1' fontSize='20px' fontFamily='Lato' fontWeight='bold'>Lady Lemon</Text>
+        <Text as='p'  fontFamily='Lato' fontWeight='bold' fontSize='18px'>By: <br/> Thomas Pringle</Text>
+        </Box>
       </SwiperSlide>
+
       <SwiperSlide><Image src='./CarouselTwo.jpeg' alt='Carousel Image'/>
-      <Text as='h1'>Skater</Text>
-        <Text as='p'>By: <br/> Peter Delira</Text>
+      
+      <Box textAlign='center'>
+      <Text as='h1' fontSize='20px'  fontFamily='Lato' fontWeight='bold'>Untitled </Text>
+        <Text as='p' fontSize='18px'>By: <br/> Gerald Wiggins</Text>
+        </Box>
             </SwiperSlide>
       <SwiperSlide>
         <Image src='CarouselThree.jpg'/>
-        <Text as='h1'>Skater</Text>
-        <Text as='p'>By: <br/> Peter Delira</Text>
+        <Box textAlign='center'>
+      <Text as='h1' fontSize='20px'  fontFamily='Lato' fontWeight='bold'>Skater</Text>
+        <Text as='p' fontSize='18px'>By: <br/> Peter Delira</Text>
+        </Box>
         </SwiperSlide>
     </Swiper>
-    </Box>
     </>
   );
 };
